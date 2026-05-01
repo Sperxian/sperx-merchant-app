66f5305799ee877174ae1579e829dccb3327364b
Pool Golez <loop.edward@gmail.com>
2026-05-01 17:47:28 +0800

    Add option to redeem reward

--- src/app/merchant/scanner/page.tsx
+++ src/app/merchant/scanner/page.tsx
@@ -10,14 +10,19 @@ const LOYALTY_CONFIG = {
   rewardLabel: "free coffee",
 };
 
+type MODE_OPTIONS = "APPLY_STAMP" | "REDEEM_REWARD";
+const options = {
+  APPLY_STAMP: { label: "Apply stamp" },
+  REDEEM_REWARD: { label: "Redeem reward" },
+};
+
 export default function QrScanPage() {
   const MEMBER_ID = "3aa24af8-1798-4af9-95bd-153dc1564cce";
 
   const [sheetOpen, setSheetOpen] = useState(false);
+  const [mode, setMode] = useState<MODE_OPTIONS>("APPLY_STAMP");
 
-  const [memberLoyalty, setMemberLoyalty] = useState<
-    MemberLoyalty | undefined
-  >();
+  const [memberLoyalty, setMemberLoyalty] = useState<MemberLoyalty>();
 
   const handleScan = async () => {
     setSheetOpen(true);
@@ -34,20 +39,44 @@ export default function QrScanPage() {
   };
 
   function handleClose() {
+    setMode("APPLY_STAMP");
     setSheetOpen(false);
   }
 
   return (
     <div className="flex flex-col h-full gap-6 max-w-xl bg-background">
-      <div className="flex-1 flex items-center justify-center relative overflow-hidden">
+      <div className="flex-1 flex flex-col gap-4 items-center justify-center relative overflow-hidden">
         {/* <QrScanner onScan={handleScan} /> */}
 
-        <button
-          onClick={handleScan}
-          className="bg-primary text-white text-md font-medium p-6 py-2 rounded-xl tracking-wide transition-all active:scale-95 hover:bg-secondary"
-        >
-          Simulate QR Scan
-        </button>
+        <div className="flex gap-2 rounded-xl">
+          {Object.entries(options).map(([key, option]) => {
+            const isActive = mode === key;
+
+            return (
+              <button
+                key={key}
+                onClick={() => setMode(key as MODE_OPTIONS)}
+                className={[
+                  "px-4 py-2 text-md font-medium rounded-lg transition-colors text-white",
+                  isActive
+                    ? "bg-primary shadow"
+                    : "bg-gray-500 hover:bg-gray-600",
+                ].join(" ")}
+              >
+                {option.label}
+              </button>
+            );
+          })}
+        </div>
+
+        <div>
+          <button
+            onClick={handleScan}
+            className="bg-primary text-white text-md font-medium p-6 py-2 rounded-xl tracking-wide transition-all active:scale-95 hover:bg-secondary"
+          >
+            Simulate QR Scan
+          </button>
+        </div>
       </div>
 
       <CustomerSheet
