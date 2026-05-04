b54293b7d91e6748c71df5cf041bf9e90abbb884
Pool Golez <loop.edward@gmail.com>
2026-05-04 18:34:13 +0800

    Add endpoint to redeem reward

--- src/lib/api/member.ts
+++ src/lib/api/member.ts
@@ -15,3 +15,11 @@ export async function addMemberLoyaltyPoints(memberId: string, points: number) {
 
   return data;
 }
+
+export async function redeemReward(memberId: string, rewardCode: string) {
+  console.log({ memberId, rewardCode })
+  const body = { rewardCode };
+  const { data } = await apiClient.post(`/shop/${SHOP_ID}/members/${memberId}/redeem`, body);
+
+  return data;
+}
