d3b848590464babaf8e474e41803f91f5263a825
Pool Golez <loop.edward@gmail.com>
2026-08-15 09:44:13 +0800

    SPX-30: Retrive members summary on page load

--- src/app/shop/[id]/members/page.tsx
+++ src/app/shop/[id]/members/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { useEffect } from "react";
+import { useShop } from "../ShopContext";
+import { getMembers } from "@/src/lib/api/member";
+
 export default function MembersPage() {
+  const shop = useShop();
+
+  useEffect(() => {
+    async function fetchMembers() {
+      const data = await getMembers(shop.id);
+      console.log({ data });
+    }
+
+    fetchMembers();
+  }, [shop]);
+
   return <div>Members Page</div>;
 }
