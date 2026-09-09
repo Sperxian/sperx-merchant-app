ef8027c1399e3cc0dab39531aa3a1c305c19781e
Pool Golez <loop.edward@gmail.com>
2026-09-09 22:13:55 +0800

    SPX-16: Add endpoint for setting up shop

--- src/lib/api/shop.ts
+++ src/lib/api/shop.ts
@@ -1,7 +1,33 @@
+import { LoyaltyProgramType, Shop } from "../types";
 import apiClient from "./client";
 
 export async function getShop(shopId: string) {
   const { data } = await apiClient.get(`/shop/${shopId}`);
 
   return data;
+}
+
+export async function setupShop(setupParams: ShopSetupParams) {
+  const { data } = await apiClient.post(`/shop/setup`, setupParams);
+
+  return data;
+}
+
+export type ShopSetupParams = {
+  shop: {
+    name: string;
+  };
+  loyaltyProgram: {
+    name: string;
+    type: LoyaltyProgramType;
+    config: {
+      stampIcon: string;
+      availableRewards: {
+        code: string;
+        name: string;
+        description?: string;
+        goalPoints: number;
+      }[];
+    };
+  }
 }
