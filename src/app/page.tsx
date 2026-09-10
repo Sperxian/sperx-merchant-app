b7e0d71bce2bba7866a8d35a8a8526ed8e39381a
Pool Golez <loop.edward@gmail.com>
2026-09-10 21:20:52 +0800

    SPX-16: Redirect to setup page if user doesn't have any shop set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import "@/src/app/globals.css";
-import { useRouter } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
 import { UserPublicMetadata } from "../types/clerk";
-import IncompleteShopConfig from "./(splash)/IncompleteShopConfig";
 import RedirectingMerchantScanner from "./(splash)/RedirectingMerchantScanner";
 
 export default function LandingPage() {
-  const router = useRouter();
-
   const { isSignedIn, user } = useUser();
   if (!isSignedIn) {
     return <RedirectingMerchantScanner />;
@@ -18,12 +15,12 @@ export default function LandingPage() {
   const { shops } = user.publicMetadata as UserPublicMetadata;
 
   if (!shops || shops.length === 0) {
-    return <IncompleteShopConfig />;
+    return redirect(`/setup`);
   }
 
   //  Redirect to first shop
   //  Currently we don't support multiple shops per user,
   //  but this is a placeholder for that future feature)
   const [{ id: shopId }] = shops;
-  router.replace(`/shop/${shopId}/scanner`);
+  return redirect(`/shop/${shopId}/scanner`);
 }
